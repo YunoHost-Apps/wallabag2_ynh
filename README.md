38a30f8b12145f69836531ed90b8b0ff3dd4c427
Jérôme Lebleu <jerome@maroufle.fr>
2016-05-22 22:16:15 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+Wallabag v2 for Yunohost
+------------------------
+
+**Warning! This is a work-in-process package... Please read the TODO below
+to see what's not done yet. Also, note that you should install this app
+for testing purpose only since there is no guarantee that the upgrade works
+at the current state.**
+
+**Shipped version:** 2.0.4
+
+[Wallabag](https://www.wallabag.org/) is a self hostable application allowing
+you to not miss any content anymore. Click, save, read it when you can. It
+extracts content so that you can read it when you have time.
+
+## TODO
+
+ * Consider using composer to fetch dependencies and retrieve Wallabag sources
+   from the git repository - as suggested in the
+   [official documentation](http://doc.wallabag.org/en/master/user/installation.html#on-a-dedicated-web-server-recommended-way)
+ * Add YunoHost users and connect authentication to the SSO
+ * Write the upgrade / backup / restore scripts
+
+**Current state:** it only installs Wallabag yet without any user.
+
+## Upgrade from v1
+
+The upgrade from the [Wallabag v1](https://github.com/YunoHost-Apps/wallabag_ynh)
+app requires a manual operation, that's why it's provided as a new package.
+For the migration process, please refer to the
+[official documentation](http://doc.wallabag.org/en/master/user/migration.html).
+
+## Limitation
+
+This application does not connect to the LDAP yet which is needed to retrieve
+users information - such as the password. One consequence is that you will
+not be able to use third-party applications which are not Web browser add-on -
+i.e. the Android application. Conversely, the add-ons for Firefox and Chrome
+will work as long as you're connected to the SSO.
+It is nevertheless a [planned feature](https://github.com/wallabag/wallabag/issues/966)
+for the v2.1 and will be provided by this package as soon as it's implemented.
+
+## Links
+
+**Wallabag**: https://www.wallabag.org/
+
+**YunoHost**: https://yunohost.org/
---
+++ scripts/_common.sh
@@ -0,0 +1,108 @@
+#
+# Common variables
+#
+
+# Wallabag version
+VERSION=2.0.4
+
+# Package name for Wallabag dependencies
+DEPS_PKG_NAME="wallabag-deps"
+
+# Wallabag git repository URL
+WALLABAG_GIT_URL="https://github.com/wallabag/wallabag.git"
+
+# Full Wallabag sources tarball URL
+WALLABAG_SOURCE_URL="https://framabag.org/wallabag-release-${VERSION}.tar.gz"
+
+# Full Wallabag sources tarball checksum
+WALLABAG_SOURCE_SHA256="ffc19e6875463ce7407934acbd98ce6c66e6feb9de0314ea47cf88e4ab007897"
+
+#
+# Common helpers
+#
+
+# Source app helpers
+. /usr/share/yunohost/helpers
+
+# Execute a command as another user
+# usage: exec_as USER COMMAND [ARG ...]
+exec_as() {
+  local USER=$1
+  shift 1
+
+  if [[ $USER = $(whoami) ]]; then
+    eval $@
+  else
+    # use sudo twice to be root and be allowed to use another user
+    sudo sudo -u "$USER" $@
+  fi
+}
+
+## Execute a composer command from a given directory
+## usage: exec_composer AS_USER WORKDIR COMMAND [ARG ...]
+#exec_composer() {
+#  local AS_USER=$1
+#  local WORKDIR=$2
+#  shift 2
+#
+#  exec_as "$AS_USER" COMPOSER_HOME="${WORKDIR}/.composer" SYMFONY_ENV=prod \
+#    php "${WORKDIR}/composer.phar" $@ \
+#      -d "${WORKDIR}" --no-interaction
+#}
+
+## Install and initialize Composer in the given directory
+## usage: init_composer DESTDIR [AS_USER]
+#init_composer() {
+#  local DESTDIR=$1
+#  local AS_USER=${2:-$USER}
+#
+#  # install composer
+#  curl -sS https://getcomposer.org/installer \
+#    | COMPOSER_HOME="${DESTDIR}/.composer" \
+#        php -- --quiet --install-dir="$DESTDIR" \
+#    || die "Unable to install Composer"
+# 
+#  # install dependencies
+#  exec_composer "$AS_USER" "$DESTDIR" install --no-dev --prefer-dist
+#}
+
+# Execute a command through the wallabag console
+# usage: exec_console AS_USER WORKDIR COMMAND [ARG ...]
+exec_console() {
+  local AS_USER=$1
+  local WORKDIR=$2
+  shift 2
+
+  (cd "$WORKDIR" && \
+   exec_as "$AS_USER" php "bin/console" --no-interaction --env=prod $@)
+}
+
+## Fetch git repository and checkout Wallabag version to the given directory
+## usage: clone_wallabag DESTDIR
+#clone_wallabag() {
+#  local DESTDIR=$1
+#
+#  # clone git repository
+#  git clone -q "$WALLABAG_GIT_URL" "$DESTDIR" \
+#    || ynh_die "Unable to fetch Wallabag sources"
+#  (cd "$DESTDIR" && git checkout -q "$VERSION") \
+#    || ynh_die "Unable to retrieve Wallabag version"
+#}
+
+# Download and extract Wallabag sources to the given directory
+# usage: extract_wallabag DESTDIR [AS_USER]
+extract_wallabag() {
+  local DESTDIR=$1
+  local AS_USER=${2:-$USER}
+
+  # retrieve and extract Roundcube tarball
+  wb_tarball="/tmp/wallabag.tar.gz"
+  rm -f "$wb_tarball"
+  wget -q -O "$wb_tarball" "$WALLABAG_SOURCE_URL" \
+    || ynh_die "Unable to download Wallabag tarball"
+  echo "$WALLABAG_SOURCE_SHA256 $wb_tarball" | sha256sum -c >/dev/null \
+    || ynh_die "Invalid checksum of downloaded tarball"
+  exec_as "$AS_USER" tar xf "$wb_tarball" -C "$DESTDIR" --strip-components 1 \
+    || ynh_die "Unable to extract Wallabag tarball"
+  rm -f "$wb_tarball"
+}
