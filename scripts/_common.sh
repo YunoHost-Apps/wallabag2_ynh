a985b7eadabdc60b6b37f588769e5e12a6cb3699
Jules Bertholet <jules.bertholet@gmail.com>
2021-04-07 11:27:15 -0400

    Check for existence of cache file before chmoding it

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -8,6 +8,22 @@
 pkg_dependencies="php-cli php-mysql php-json php-gd php-tidy php-curl php-gettext php-redis"
 
 #=================================================
+# PERSONAL HELPERS
+#=================================================
+
+function set_permissions {
+  # Set permissions to app files
+  chown -R $app:www-data $final_path
+  chmod -R g=u,g-w,o-rwx $final_path
+
+  # Restrict rights to Wallabag user only
+  chmod 600 $wb_conf
+  if [ -e $final_path/var/cache/prod/appProdProjectContainer.php ]; then
+    chmod 700 $final_path/var/cache/prod/appProdProjectContainer.php
+  fi
+}
+
+#=================================================
 # EXPERIMENTAL HELPERS
 #=================================================
 
