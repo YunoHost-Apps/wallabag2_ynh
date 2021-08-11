73f23104d6c4d6f3fcfc0add7f810665911991d3
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2021-08-11 11:49:20 +0200

    Fix PHP version and other things (#109)
    Co-authored-by: @ericgaspar 
    Co-authored-by: lapineige <lapineige@users.noreply.github.com>

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,38 +4,21 @@
 # COMMON VARIABLES
 #=================================================
 
+YNH_PHP_VERSION="7.3"
+
 # dependencies used by the app
-pkg_dependencies="php$YNH_DEFAULT_PHP_VERSION-cli php$YNH_DEFAULT_PHP_VERSION-mysql php$YNH_DEFAULT_PHP_VERSION-json php$YNH_DEFAULT_PHP_VERSION-gd php$YNH_DEFAULT_PHP_VERSION-tidy php$YNH_DEFAULT_PHP_VERSION-curl php$YNH_DEFAULT_PHP_VERSION-php-gettext php$YNH_DEFAULT_PHP_VERSION-redis"
+extra_php_dependencies="php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-json php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-tidy php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-gettext php${YNH_PHP_VERSION}-redis"
+
+pkg_dependencies="redis-server redis-tools"
 
 #=================================================
 # PERSONAL HELPERS
 #=================================================
 
-function set_permissions {
-  # Set permissions to app files
-  chown -R $app:www-data $final_path
-  chmod -R g=u,g-w,o-rwx $final_path
-
-  # Restrict rights to Wallabag user only
-  chmod 600 $wb_conf
-  if [ -e $final_path/var/cache/prod/appProdProjectContainer.php ]; then
-    chmod 700 $final_path/var/cache/prod/appProdProjectContainer.php
-  fi
-}
-
 #=================================================
 # EXPERIMENTAL HELPERS
 #=================================================
 
-# Execute a command as another user
-# usage: exec_as USER COMMAND [ARG ...]
-ynh_exec_as() {
-  local USER=$1
-  shift 1
-
-  if [[ $USER = $(whoami) ]]; then
-    eval "$@"
-  else
-    sudo -u "$USER" "$@"
-  fi
-}
+#=================================================
+# FUTURE OFFICIAL HELPERS
+#=================================================
