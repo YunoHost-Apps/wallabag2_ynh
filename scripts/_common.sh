3bd6b4c406249c69e2922e2eff8525b7352f9a87
Gofannon <17145502+Gofannon@users.noreply.github.com>
2022-06-04 00:39:43 +0200

    set php to version 7.3

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,6 +4,10 @@
 # COMMON VARIABLES
 #=================================================
 
+# Overload "default php" version to bypass package limitation not ready to use 7.4 yet (version 2.4.0 minimun. See https://github.com/wallabag/wallabag/blob/master/CHANGELOG.md#240)
+# Fallback to php 7.3 as on debian 11 / bulleyes, 7.4 is used by default
+YNH_DEFAULT_PHP_VERSION=7.3
+
 # dependencies used by the app
 pkg_dependencies="php$YNH_DEFAULT_PHP_VERSION-cli php$YNH_DEFAULT_PHP_VERSION-mysql php$YNH_DEFAULT_PHP_VERSION-json php$YNH_DEFAULT_PHP_VERSION-gd php$YNH_DEFAULT_PHP_VERSION-tidy php$YNH_DEFAULT_PHP_VERSION-curl php$YNH_DEFAULT_PHP_VERSION-php-gettext php$YNH_DEFAULT_PHP_VERSION-redis"
 
