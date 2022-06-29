90000f5ba5a2de2bb2fcea7460f4cd88006ba6a6
Gofannon <17145502+Gofannon@users.noreply.github.com>
2022-06-29 20:19:13 +0200

    Use YNH_PHP_VERSION as it seems the way to go

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,20 +6,27 @@
 
 # Overload "default php" version to bypass package limitation not ready to use 7.4 yet (version 2.4.0 minimun. See https://github.com/wallabag/wallabag/blob/master/CHANGELOG.md#240)
 # Fallback to php 7.3 as on debian 11 / bulleyes, 7.4 is used by default
-YNH_DEFAULT_PHP_VERSION=7.3
+YNH_PHP_VERSION=7.3
 
 # dependencies used by the app
 pkg_dependencies="
-php$YNH_DEFAULT_PHP_VERSION-cli
-php$YNH_DEFAULT_PHP_VERSION-mysql
-php$YNH_DEFAULT_PHP_VERSION-json
-php$YNH_DEFAULT_PHP_VERSION-gd
-php$YNH_DEFAULT_PHP_VERSION-tidy
-php$YNH_DEFAULT_PHP_VERSION-curl
-php$YNH_DEFAULT_PHP_VERSION-gettext
-php$YNH_DEFAULT_PHP_VERSION-redis
-php$YNH_DEFAULT_PHP_VERSION-common
+php$YNH_PHP_VERSION-cli
+php$YNH_PHP_VERSION-mysql
+php$YNH_PHP_VERSION-json
+php$YNH_PHP_VERSION-gd
+php$YNH_PHP_VERSION-tidy
+php$YNH_PHP_VERSION-curl
+php$YNH_PHP_VERSION-gettext
+php$YNH_PHP_VERSION-redis
+php$YNH_PHP_VERSION-common
 "
+
+### sources for php version overload:
+# https://github.com/YunoHost-Apps/wallabag2_ynh/pull/128#pullrequestreview-986708050=
+# https://github.com/YunoHost-Apps/grav_ynh/blob/testing/scripts/_common.sh
+# https://github.com/YunoHost/example_ynh/blob/master/scripts/_common.sh
+# https://github.com/YunoHost/yunohost/blob/dev/helpers/php#L3-L6=
+###
 
 #=================================================
 # PERSONAL HELPERS
