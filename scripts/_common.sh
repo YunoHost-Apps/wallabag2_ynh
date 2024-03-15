4c2f0ef2313128a628276bf83bd2f0c11c396ee0
Félix Piédallu <felix@piedallu.me>
2024-03-15 12:51:07 +0100

    Some cleanup, to reduce diff with old_testing

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -10,18 +10,6 @@ wb_conf="$install_dir/app/config/parameters.yml"
 # PERSONAL HELPERS
 #=================================================
 
-function set_permissions {
-  # Set permissions to app files
-  chown -R $app:www-data $install_dir
-  chmod -R g=u,g-w,o-rwx $install_dir
-
-  # Restrict rights to Wallabag user only
-  chmod 600 $wb_conf
-  if [ -e $install_dir/var/cache/prod/appProdProjectContainer.php ]; then
-    chmod 700 $install_dir/var/cache/prod/appProdProjectContainer.php
-  fi
-}
-
 #=================================================
 # EXPERIMENTAL HELPERS
 #=================================================
