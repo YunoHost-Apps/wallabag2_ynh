53c51477fab31438c8b80f4d82c32efb976384f3
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2024-07-20 11:53:43 +0200

    add function

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,6 +4,12 @@
 # COMMON VARIABLES
 #=================================================
 
+# Define a function to execute commands with `php_exec`
+php_exec() {
+  (cd "$install_dir" && ynh_exec_as "$app" \
+      php${phpversion} "$install_dir/bin/console" --no-interaction --env=prod "$@")
+}
+
 #=================================================
 # PERSONAL HELPERS
 #=================================================
