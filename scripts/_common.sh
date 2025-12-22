5ce1bc88fd85250279b8aec34fb92ebcc5410715
eric_G <46165813+ericgaspar@users.noreply.github.com>
2025-12-22 11:05:21 +0100

    cleaning

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,8 +4,6 @@
 # COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
 
-composer_version="2.7.7"
-
 # Define a function to execute commands with `php_exec`
 php_exec() {
   (cd "$install_dir" && ynh_exec_as_app \
